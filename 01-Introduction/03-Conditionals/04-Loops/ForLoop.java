85f1fd5f3f4648944f1115ac31acbf89b5fb2cb8
Asif Ahmed Shaik <asifahmedshaik18@gmail.com>
2026-07-30 11:45:19 +0530

    Add Java loops examples

---
+++ 01-Introduction/03-Conditionals/04-Loops/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        System.out.println("Number from 1 to 10:");
+
+        for(int i = 0;i <= 10;i++) {
+            System.out.println(i);
+
+        }
+            
+    }
+}
+
---
+++ 01-Introduction/03-Conditionals/04-Loops/Patterns.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Patterns {
+
+    public static void main(String[] args) {
+
+        for(int i = 5;i >= 1;i--) {
+            for (int j = 5;j >= i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+
+        }
+    }
+}
