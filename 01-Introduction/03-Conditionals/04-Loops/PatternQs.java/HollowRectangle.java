85a64140ba959351a905b400cea1a2829ddf36c7
Asif Ahmed Shaik <asifahmedshaik18@gmail.com>
2026-07-31 10:18:07 +0530

    Add basic patterns Examples 2.0

--- 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/HollowRectangle.java
+++ 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/HollowRectangle.java
@@ -1,4 +1,18 @@
 public class HollowRectangle {
+    public static void main(String[] args) {
+        
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<4; j++) {
+                if(i == 0 || i == 5 - 1 || j == 0 || j == 4 - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
     
 }
   
---
+++ 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/InvertedHPRotated.java
@@ -0,0 +1,15 @@
+public class InvertedHPRotated {
+    public static void main(String[] args) {
+
+        for(int i = 4; i>=1; i--) {
+            for(int j=1; j<i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<=4-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
