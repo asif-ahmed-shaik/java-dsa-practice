d1a34cda5d6863a1ed5ff3a158c921033614378b
Asif Ahmed Shaik <asifahmedshaik18@gmail.com>
2026-08-02 17:11:03 +0530

    Add function concepts basic examples

---
+++ 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/05-Functions/BasicCalculator.java
@@ -0,0 +1,27 @@
+public class BasicCalculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Addition       : " + add(10, 5));
+        System.out.println("Subtraction    : " + subtract(10, 5));
+        System.out.println("Multiplication : " + multiply(10, 5));
+        System.out.println("Division       : " + divide(10, 5));
+
+    }
+}
---
+++ 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/05-Functions/FunctionBasics.java
@@ -0,0 +1,14 @@
+public class FunctionBasics {
+
+    
+    public static void printMessage() {
+        System.out.println("Welcome to Java!");
+        System.out.println("Methods make code reusable.");
+    }
+    public static void main(String[] args) {
+
+        printMessage();
+        printMessage();
+
+    }
+}
