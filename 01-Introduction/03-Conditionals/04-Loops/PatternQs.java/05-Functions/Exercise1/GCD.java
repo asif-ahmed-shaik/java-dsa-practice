5701141aff70fbf5237e086436c78af3316fac09
Asif Ahmed Shaik <asifahmedshaik18@gmail.com>
2026-08-03 20:32:19 +0530

    Add GCD in functions concept example

---
+++ 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/05-Functions/Exercise1/GCD.java
@@ -0,0 +1,22 @@
+package Exercise1;
+import java.util.*;
+
+public class GCD {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while(a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println("GCD is:" + b);
+    }
+
+}
+    
