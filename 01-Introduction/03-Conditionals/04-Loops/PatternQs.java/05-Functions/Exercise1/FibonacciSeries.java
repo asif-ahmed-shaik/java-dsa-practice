9744982a03e67c4d8a7fee3f0ee6bc53f31f96ed
Asif Ahmed Shaik <asifahmedshaik18@gmail.com>
2026-08-04 23:15:56 +0530

    Add Java examples for time and space complexity

--- 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/05-Functions/Exercise1/FibonacciSeries.java
+++ 01-Introduction/03-Conditionals/04-Loops/PatternQs.java/05-Functions/Exercise1/FibonacciSeries.java
@@ -8,11 +8,11 @@ public class FibonacciSeries {
 
         int a = 0, b = 1;
 
-        System.out.println(a +" ");
+        System.out.print(a +" ");
 
         if (n > 1) {
             for(int i = 2;i <= n;i++) {
-                System.out.println(b+" ");
+                System.out.print(b+" ");
 
                 int temp = b;
                 b = a + b;
